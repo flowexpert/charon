f162f4287b93989a3c70889181ed8d819c9e93a0
piranha87 <>
2009-09-02 09:10:15 +0000

    added code for birghtnessmodelplugins

---
+++ Exponential.cpp
@@ -0,0 +1,21 @@
+#define TYPE BrightnessModels::Exponential
+
+#include "Exponential.hxx"
+
+#if defined(_MSC_VER) || defined(WIN32)  || defined(_WIN32) || defined(__WIN32__) \
+   || defined(WIN64)    || defined(_WIN64) || defined(__WIN64__)
+#define DECLDIR __declspec(dllexport)
+#else
+#define DECLDIR ""
+#endif
+
+typedef unsigned short int template_type;
+
+extern "C" DECLDIR ParameteredObject * create(std::string name) {
+	return new TYPE<int>(name);
+}
+
+extern "C" DECLDIR void destroy(ParameteredObject * b) {
+	delete b;
+}
+
