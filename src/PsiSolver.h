53b3b763b4bba928d9df50b43ecea38ce1cd656d
techfreaq <>
2009-12-02 16:42:42 +0000

    PsiSolver added

---
+++ src/PsiSolver.h
@@ -0,0 +1,86 @@
+/*  This file is part of Charon.
+
+    Charon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Charon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file Solver.h
+ *  Implementation of class PsiSolver.
+ *  @author <a href="mailto:techfreaq@web.de">
+ *      Nina Hernitschek</a>
+ *  @date 23.11.2009
+ */
+
+//
+//
+//#ifndef _PSISOLVER_H_
+//#define _PSISOLVER_H_
+//
+//#if defined(MSVC) && defined(HANDLE_DLL)
+//#ifdef psisolver_EXPORTS
+///// Visual C++ specific code
+//#define psisolver_DECLDIR __declspec(dllexport)
+//#else
+//#define psisolver_DECLDIR __declspec(dllimport)
+//#endif /*Export or import*/
+//
+//
+//#else /* No DLL handling or GCC */
+///// Not needed with GCC
+//#define psisolver_DECLDIR
+//#endif
+//
+//#include <iostream>
+//#include <cstdlib>
+//#include "ParameteredObject.hxx"
+//
+//class sample_DECLDIR Sample : public ParameteredObject {
+//
+//	/// CImgList containing the result.
+//	cimg_library::CImgList<T> result;
+//
+//	public:
+//	/*/// pointers, that the solver will use
+//	InputSlot< Stencil<T>* > stencils;
+//
+//	/// region of interes for the solver to work on
+//	InputSlot< Roi<int>* > roi;*/
+//
+//	InputSlot<cimg_library::CImgList<T> > imgin;
+//
+//	/// result
+//	OutputSlot<cimg_library::CImgList<T> > imgout;
+//
+//	/// default constructor
+//	PsiSolver(
+//		const std::string& classname,	///< [in] class name
+//		const std::string& name = ""	///< [in] instance name
+//	);
+//
+//	
+//	/// encapsulated execute function 
+//	int PsiSolverExecute();
+//
+//	/// main function
+//	virtual void execute();
+//
+//	//returns Psi
+//	void CalcPsi();
+//	//returns derivative of Psi
+//	void CalcDPsi();
+//	/// default destructor
+//	virtual ~PsiSolver();
+//
+//};
+//
+//
+//#endif // _PSISOLVER_H_
---
+++ src/PsiSolver.hxx
@@ -0,0 +1,40 @@
+/*  This file is part of Charon.
+
+    Charon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Charon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file Solver.hxx
+ *  Implementation of class Solver.
+ *  @author <a href="mailto:techfreaq@web.de">
+ *      Nina Hernitschek</a>
+ *  @date 23.11.2009
+ */
+//
+//#ifndef _PSISOLVER_HXX_
+//#define _PSISOLVER_HXX_
+//
+//#include "PsiSolver.h"
+//
+//template <typename T>
+//Solver<T>::Solver(const std::string& classname, const std::string& name) :
+//		TemplatedParameteredObject<T>(classname,name,"solves the linear system")
+//{
+//	this->_addInputSlot(imgin,"imgin","CImgList","CImgList<T>");
+//	this->_addOutputSlot(imgout,"imgout","CImgList containing the solution","CImgList<T>");
+//	out() = result;
+//}
+//
+//template <typename T>
+//Solver<T>::~Solver() {}
+//
+//#endif // _PSISOLVER_HXX_
