bf0e408f90c0922e55b7fba18421f9abf01a018a
jmgottfried <>
2010-01-22 11:52:14 +0000

    applied coding conventions to SplitStream header

--- src/SplitStream.h
+++ src/SplitStream.h
@@ -13,11 +13,11 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Charon.  If not, see <http://www.gnu.org/licenses/>.
 */
-/**	@file SplitStream.h
- *	Declaration of class SplitStream.
- *	@author <a href="mailto:jmgottfried@web.de">Jens-Malte Gottfried</a>
- *	@date 11.08.2008
- *	@remark	Used Interface from SplitStream Class of Daniel Kondermann
+/** @file SplitStream.h
+ *  Declaration of class SplitStream.
+ *  @author  <a href="mailto:jmgottfried@web.de">Jens-Malte Gottfried</a>
+ *  @date    11.08.2008
+ *  @remark  Used Interface from SplitStream Class of Daniel Kondermann
  */
 
 #ifndef _SPLIT_STREAM_H_
@@ -55,49 +55,50 @@ public:
 	SplitStreamBuf(const std::vector<std::streambuf*>& buffers);
 	virtual ~SplitStreamBuf();
 
-	/**	Write character in the case of overflow.
-	 *	@param c		char value to check
-	 *	@return			EOF if overflow occured
+	/** Write character in the case of overflow.
+	 *  @param c        char value to check
+	 *  @return         EOF if overflow occured
 	 */
 	int overflow(int c);
 
-	/**	Write sequence of characters.
-	 *	@param str		char array to put
-	 *	@param size		number of characters
-	 *	@return			number of written characters
+	/** Write sequence of characters.
+	 *  @param str      char array to put
+	 *  @param size     number of characters
+	 *  @return         number of written characters
 	 */
 	std::streamsize xsputn(char const * str, std::streamsize size);
 
-	/**	Sync stream buffers.
-	 *	@return			EOF on failure
+	/** Sync stream buffers.
+	 *  @return         EOF on failure
 	 */
 	int sync();
 };
 
-/**	This class wraps a list of output streams and
- *	pipes output made to a splitstream instance to
- *	all of these output streams.
+/** This class wraps a list of output streams and
+ *  pipes output made to a splitstream instance to
+ *  all of these output streams.
  */
 class DLLEX SplitStream : public std::ostream {
 public:
-	/**	Default constructor.
-	 *	Use no output stream.
+	/// Default constructor
+	/** Use no output stream.<br>
+	 *  Make sure you call assign() before using this SplitStream.
 	 */
 	SplitStream();
 
-	/**	Assign one stream to output list.
-	 *	@param stream		stream to pipe output to
+	/** Assign one stream to output list.
+	 *  @param stream       stream to pipe output to
 	 */
 	SplitStream(std::ostream& stream);
 
-	/**	Assign two streams to output list.
-	 *	@param stream1		fist output stream to assign
-	 *	@param stream2		second stream to assign
+	/** Assign two streams to output list.
+	 *  @param stream1      fist output stream to assign
+	 *  @param stream2      second stream to assign
 	 */
 	SplitStream(std::ostream& stream1, std::ostream& stream2);
 
-	/**	Assing a whole list of streams.
-	 *	@param streamList	list of streams to assign
+	/** Assing a whole list of streams.
+	 *  @param streamList   list of streams to assign
 	 */
 	SplitStream(std::vector<std::ostream*>& streamList);
 
@@ -112,7 +113,9 @@ public:
 	//@}
 
 	/** Dummy function to fulfill interface
-	 *	@return				always true
+	 *  \deprecated         This function is not used in current plugins,
+	 *                      check if it's still needed.
+	 *  \return             always true
 	 */
 	bool isZeroRank();
 
