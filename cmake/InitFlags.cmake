eb091768803092f63abaeb70f2d8838ea1cac149
jmgottfried <>
2010-03-16 18:17:51 +0000

    prepared tuchulcha for usage in charon-meta

--- cmake/InitFlags.cmake
+++ cmake/InitFlags.cmake
@@ -4,36 +4,35 @@ IF(NOT CMAKE_FLAGS_INIT)
 	# executed once in the first configure run.
 	IF(CMAKE_COMPILER_IS_GNUCXX)
 		IF(NOT CMAKE_C_FLAGS)
-			SET(CMAKE_C_FLAGS
-				"-W -Wall -Wextra"
-				CACHE STRING
-				"Flags used by the compiler during all build types."
-				FORCE
-			)
+			SET(CMAKE_C_FLAGS "-W -Wall -Wextra")
 		ENDIF(NOT CMAKE_C_FLAGS)
 		IF(NOT CMAKE_CXX_FLAGS)
-			SET(CMAKE_CXX_FLAGS
-				"-W -Wall -Wextra"
-				CACHE STRING
-				"Flags used by the compiler during all build types."
-				FORCE
-			)
+			SET(CMAKE_CXX_FLAGS	"-W -Wall -Wextra")
 		ENDIF(NOT CMAKE_CXX_FLAGS)
 	ENDIF(CMAKE_COMPILER_IS_GNUCXX)
 	IF(MSVC)
-		SET(CMAKE_C_FLAGS
-			"${CMAKE_C_FLAGS} /wd4251 /wd4290"
-			CACHE STRING
-			"Flags used by the compiler during all build types."
-			FORCE
-		)
-		SET(CMAKE_CXX_FLAGS
-			"${CMAKE_CXX_FLAGS} /wd4251 /wd4290"
-			CACHE STRING
-			"Flags used by the compiler during all build types."
-			FORCE
-		)
+		SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4251 /wd4275 /wd4290")
+		SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4251 /wd4275 /wd4290")
 	ENDIF(MSVC)
+
+	# commit changed flags
+	SET(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} CACHE STRING
+		"Flags used by the compiler during all build types."
+		FORCE
+	)
+	SET(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING
+		"Flags used by the compiler during all build types."
+		FORCE
+	)
+
+	# default to debug builds
+	IF(DEFINED CMAKE_BUILD_TYPE AND CMAKE_BUILD_TYPE STREQUAL "")
+		SET(CMAKE_BUILD_TYPE "Debug"
+			CACHE STRING
+			"Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
+			FORCE
+		)
+	ENDIF(DEFINED CMAKE_BUILD_TYPE AND CMAKE_BUILD_TYPE STREQUAL "")
 	# set install prefix to global install path
 	IF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
 		SET(CMAKE_INSTALL_PREFIX ${CHARON_CORE_ROOT_DIR}
@@ -44,14 +43,6 @@ IF(NOT CMAKE_FLAGS_INIT)
 	ELSE(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
 		MESSAGE(STATUS "Using user-specified install prefix: ${CMAKE_INSTALL_PREFIX}")
 	ENDIF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-	# default to debug builds
-	IF(DEFINED CMAKE_BUILD_TYPE AND CMAKE_BUILD_TYPE STREQUAL "")
-		SET(CMAKE_BUILD_TYPE "Debug"
-			CACHE STRING
-			"Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
-			FORCE
-		)
-	ENDIF(DEFINED CMAKE_BUILD_TYPE AND CMAKE_BUILD_TYPE STREQUAL "")
 ENDIF(NOT CMAKE_FLAGS_INIT)
 
 # set definitions depending on build environment
@@ -59,7 +50,7 @@ IF(WIN32)
 	ADD_DEFINITIONS(-DWINDOWS)
 	IF(MSVC)
 		ADD_DEFINITIONS(-DMSVC)
-		#Tell MSVC not to use the unholy min and max macros
+		# Tell MSVC not to use the unholy min and max macros
 		ADD_DEFINITIONS(-DNOMINMAX)
 	ENDIF(MSVC)
 	IF(MINGW)
@@ -72,4 +63,3 @@ IF(UNIX)
 		ADD_DEFINITIONS(-DAPPLE)
 	ENDIF(APPLE)
 ENDIF(UNIX)
-ADD_DEFINITIONS(-Dcimg_verbosity=0)
