335ea1eef5f00d2327879917c1fc24e0ac129f84
Jens-Malte Gottfried <jmgottfried@web.de>
2010-11-22 12:10:55 +0100

    added refactored DataConstant stencil (was DataBC in charon-learning) with improved doc

---
+++ src/DataConstant.cpp
@@ -0,0 +1,51 @@
+/*  Copyright (C) 2010 Jens-Malte Gottfried
+
+	This file is part of Charon.
+
+	Charon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Charon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** \file Diff2Function.h
+ *  Declaration of the abstract interface class Diff2Function.
+ *  \author <a href="Jens-Malte.Gottfried@iwr.uni-heidelberg.de>
+ *      Jens-Malte Gottfried </a>
+ *  \date 18.01.2010
+ */
+#define TYPE DataConstant
+
+#if defined(MSVC) && defined (databc_EXPORTS)
+#define stencil_EXPORTS
+#endif
+#include <charon/Stencils/DataConstant.hxx>
+
+extern "C" dataconstant_DECLDIR ParameteredObject*
+		create(const std::string &name, template_type t) {
+	switch(t) {
+	case ParameteredObject::TYPE_DOUBLE:
+		return new TYPE<double>(name);
+		break;
+	case ParameteredObject::TYPE_FLOAT:
+		return new TYPE<float>(name);
+		break;
+	case ParameteredObject::TYPE_INT:
+		return new TYPE<int>(name);
+		break;
+	default:
+		return new TYPE<int>(name);
+		break;
+	}
+}
+
+extern "C" dataconstant_DECLDIR void destroy(ParameteredObject * b) {
+	delete b;
+}
