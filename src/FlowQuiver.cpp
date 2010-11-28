22939582b2ecd421cfed2325dbbdf6e5807bd6bc
Jens-Malte Gottfried <jmgottfried@web.de>
2010-11-28 01:25:31 +0100

    added FlowQuiver

---
+++ src/FlowQuiver.cpp
@@ -0,0 +1,52 @@
+/*  Copyright (C) 2009 Jens-Malte Gottfried
+
+	This file is part of Charon.
+
+	Charon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Charon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/// \file FlowQuiver.cpp
+/// Needed for class FlowQuiver to work as charon plugin.
+/// \author <a href="mailto:Jens-Malte.Gottfried@iwr.uni-heidelberg.de">
+///     Jens-Malte Gottfried</a>
+/// \date 27.11.2010
+
+#include <charon-utils/FlowQuiver.hxx>
+
+/// Class name of the plugin
+#define TYPE FlowQuiver
+
+/// Creates an instance of the plugin
+extern "C" flowquiver_DECLDIR
+ParameteredObject* create(const std::string& name, template_type t) {
+	switch(t) {
+	case ParameteredObject::TYPE_DOUBLE:
+		return new TYPE<double>(name);
+		break;
+	case ParameteredObject::TYPE_FLOAT:
+		return new TYPE<float>(name);
+		break;
+	case ParameteredObject::TYPE_INT:
+		return new TYPE<int>(name);
+		break;
+	default:
+		return new TYPE<int>(name);
+		break;
+	}
+}
+
+/// Deletes an instance of the plugin
+extern "C" flowquiver_DECLDIR
+void destroy(ParameteredObject * b) {
+	delete b;
+}
