9c8bc156a0fb5e0c7564be16e07f667d41ab4c82
Jens-Malte Gottfried <jmgottfried@web.de>
2011-10-17 15:35:25 +0200

    fixed copyright header, added manpage

--- app/xmlModuleDetector.cpp
+++ app/xmlModuleDetector.cpp
@@ -1,3 +1,26 @@
+/*  Copyright (C) 2011 Jens-Malte Gottfried
+
+	This file is part of Charon.
+
+	Charon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Charon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** \file xmlModuleDetector.hxx
+ *  Helper application to detect charon modules in doxygen generated xml
+ *  files.
+ *  \author <a href="mailto:jmgottfried@web.de>Jens-Malte Gottfried</a>
+ *  \date 17.10.2011
+ */
 #include <QtXml>
 #include <stdexcept>
 #include <QTextStream>
