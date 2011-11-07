75b01090e3f3d41ff357a80ece64005a010c80b5
jmgottfried <>
2011-11-07 13:17:19 +0000

    fixed EdgySequenceGenerator.cpp

--- src/EdgySequenceGenerator.cpp
+++ src/EdgySequenceGenerator.cpp
@@ -24,9 +24,6 @@
 /// Class name of the plugin
 #define TYPE EdgySequenceGenerator
 
-#ifndef _MSC_VER
-#include <charon-utils/Interpolator.hxx>
-#endif
 #include <charon/EdgySequenceGenerator.hxx>
 
 /// Creates an instance of the plugin
