ba9d4c74fd259ef2ccacdcfdb5ff109645754c19
Stephan Meister <stephan.meister@iwr.uni-heidelberg.de>
2011-11-16 13:54:34 +0100

    added getBuildType function to cpp templates

--- src/res/Temp.cpp
+++ src/res/Temp.cpp
@@ -49,3 +49,12 @@ extern "C" @modulename@_DECLDIR ParameteredObject*
 extern "C" @modulename@_DECLDIR void destroy(ParameteredObject * b) {
 	delete b;
 }
+
+/// Report build configuration to prevent linking of incompatibel runtime libs
+extern "C" @modulename@_DECLDIR ParameteredObject::build_type getBuildType() {
+#ifdef _DEBUG
+	return ParameteredObject::DEBUG_BUILD;
+#else
+	return ParameteredObject::RELEASE_BUILD;
+#endif
+}
