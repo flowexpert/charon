bf51924939ee0fe3fe3843b7264a0936f684b88a
Stephan Meister <stephan.meister@iwr.uni-heidelberg.de>
2012-02-06 17:25:12 +0100

    added Size plugin for creating ROIs from existing images

---
+++ src/Size.cpp
@@ -0,0 +1,62 @@
+/*  Copyright (C) 2012 Stephan Meister
+
+	This file is part of Charon.
+
+	Charon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Charon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** \file Size.cpp
+ *  This file is needed for class Size to work as a charon plugin.
+ *  \author <a href="mailto:stephan.meister@iwr.uni-heidelberg.de">
+ *      Stephan Meister</a>
+ *  \date 03.02.2012
+ */
+
+/// Class name of the plugin
+#define TYPE Size
+
+#include <charon-utils/Size.hxx>
+#include <charon-utils/Roi.hxx>
+
+/// Creates an instance of the plugin
+extern "C" size_DECLDIR ParameteredObject*
+		create(const std::string & name, template_type t) {
+	switch(t) {
+	case ParameteredObject::TYPE_DOUBLE:
+		return new TYPE<double>(name);
+		break;
+	case ParameteredObject::TYPE_FLOAT:
+		return new TYPE<float>(name);
+		break;
+	case ParameteredObject::TYPE_INT:
+		return new TYPE<int>(name);
+		break;
+	default:
+		return new TYPE<int>(name);
+		break;
+	}
+}
+
+/// Deletes an instance of the plugin
+extern "C" size_DECLDIR void destroy(ParameteredObject * b) {
+	delete b;
+}
+
+/// Report build configuration to prevent linking of incompatibel runtime libs
+extern "C" size_DECLDIR ParameteredObject::build_type getBuildType() {
+#ifdef _DEBUG
+	return ParameteredObject::DEBUG_BUILD;
+#else
+	return ParameteredObject::RELEASE_BUILD;
+#endif
+}
