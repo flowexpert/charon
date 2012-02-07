0c11d510d801b2ef5ac3b79a55e11decb70a99e2
Michael Baron <michael.baron@iwr.uni-heidelberg.de>
2012-02-07 18:08:34 +0100

    solution to Hessian(Energy) * x = Gradient(Energy) using Cramer_s rule

--- include/charon/Solvers/Newton2.hxx
+++ include/charon/Solvers/Newton2.hxx
@@ -61,11 +61,11 @@ void Newton2<T>::execute() {
 
 	std::vector<T> tmp2( 2, T(0.0) );
 	std::vector<T> tmp4( 4, T(0.0) );
-	std::vector<T> localGradient( 2, T(0.0) );
-	std::vector<T> localHessian( 4, T(0.0) );
-	cimg_library::CImg<T> matHessian( 2, 2, 1, 1, T(0.0) );
-	cimg_library::CImg<T> vecGradient( 1, 2, 1, 1, T(0.0) );
-	cimg_library::CImg<T> vecSolution( 1, 2, 1, 1, T(0.0) );
+	std::vector<T> gradient( 2, T(0.0) );
+	std::vector<T> hessian( 4, T(0.0) );
+
+	double num = T(0.0);
+	double den = T(0.0);
 
 	int x, y, z;
 	Stencil::Base<T> *is;
@@ -74,36 +74,34 @@ void Newton2<T>::execute() {
 	for (y=_roi.yBegin; y<_roi.yEnd; ++y)
 	for (z=_roi.zBegin; z<_roi.zEnd; ++z)
 	{
-		localGradient[0] = T(0.0);
-		localGradient[1] = T(0.0);
-		localHessian[0] = T(0.0);
-		localHessian[1] = T(0.0);
-		localHessian[2] = T(0.0);
-		localHessian[3] = T(0.0);
+		gradient[0] = T(0.0);
+		gradient[1] = T(0.0);
+		hessian[0]  = T(0.0);
+		hessian[1]  = T(0.0);
+		hessian[2]  = T(0.0);
+		hessian[3]  = T(0.0);
 		for (sIt =  this->stencils.begin();
 		     sIt != this->stencils.end();
 		     sIt ++)
 		{
 			is = (*((InputSlot<Stencil::Base<T>*>*)*sIt))();
 			tmp2 = dynamic_cast<Stencil::EnergyGradient<T>*>(is)->getEnergyGradient( 0, x, y, z, 0 );
-			localGradient[0] += tmp2[0];
-			localGradient[1] += tmp2[1];
 			tmp4 = dynamic_cast<Stencil::EnergyHessian<T>*>(is)->getEnergyHessian( 0, x, y, z, 0 );
-			localHessian[0]  += tmp4[0];
-			localHessian[1]  += tmp4[1];
-			localHessian[2]  += tmp4[2];
-			localHessian[3]  += tmp4[3];
+			gradient[0] += tmp2[0];
+			gradient[1] += tmp2[1];
+			hessian[0]  += tmp4[0];
+			hessian[1]  += tmp4[1];
+			hessian[2]  += tmp4[2];
+			hessian[3]  += tmp4[3];
 		}
-		vecGradient(0,0,0,0) = localGradient[0];
-		vecGradient(0,1,0,0) = localGradient[1];
-		matHessian(0,0,0,0) = localHessian[0];
-		matHessian(0,1,0,0) = localHessian[1];
-		matHessian(1,0,0,0) = localHessian[2];
-		matHessian(1,1,0,0) = localHessian[3];
 
-		vecSolution = vecGradient.solve( matHessian );
-		_result( 0, x, y, z, 0 ) = -vecSolution(0,0,0,0);
-		_result( 1, x, y, z, 0 ) = -vecSolution(0,1,0,0);
+		// solution to Hessian(Energy) * Delta = Gradient(Energy)
+		// wrt Delta using Cramer's rule
+		den = hessian[1]  * hessian[2] - hessian[0]  * hessian[3] ;
+		num = gradient[0] * hessian[3] - gradient[1] * hessian[1] ;
+		_result( 0, x, y, z, 0 ) = num/den;
+		num = gradient[1] * hessian[0] - gradient[0] * hessian[2] ;
+		_result( 1, x, y, z, 0 ) = num/den;
 	}
 
 	result() = _result;
