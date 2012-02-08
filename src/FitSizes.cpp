e27532a6aff06333d472b0d85b0f04fde0bc947b
Stephan Meister <stephan.meister@iwr.uni-heidelberg.de>
2012-02-08 11:00:54 +0100

    added FitSizes plugin by Daniel

---
+++ src/FitSizes.cpp
@@ -0,0 +1,61 @@
+/*  Copyright (C) 2011 Daniel Kondermann
+
+	This file is part of Charon.
+
+	Charon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Charon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Charon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** \file FitSizes.cpp
+ *  This file is needed for class FitSizes to work as a charon plugin.
+ *  \author <a href="mailto:daniel@kondermann.de">
+ *      Daniel Kondermann</a>
+ *  \date 05.08.2011
+ */
+
+/// Class name of the plugin
+#define TYPE FitSizes
+
+#include <charon-utils/FitSizes.hxx>
+
+/// Creates an instance of the plugin
+extern "C" fitsizes_DECLDIR ParameteredObject*
+		create(const std::string & name, template_type t) {
+	switch(t) {
+	case ParameteredObject::TYPE_DOUBLE:
+		return new TYPE<double>(name);
+		break;
+	case ParameteredObject::TYPE_FLOAT:
+		return new TYPE<float>(name);
+		break;
+	case ParameteredObject::TYPE_INT:
+		return new TYPE<int>(name);
+		break;
+	default:
+		return new TYPE<int>(name);
+		break;
+	}
+}
+
+/// Deletes an instance of the plugin
+extern "C" fitsizes_DECLDIR void destroy(ParameteredObject * b) {
+	delete b;
+}
+
+/// Report build configuration to prevent linking of incompatibel runtime libs
+extern "C" fitsizes_DECLDIR ParameteredObject::build_type getBuildType() {
+#ifdef _DEBUG
+	return ParameteredObject::DEBUG_BUILD;
+#else
+	return ParameteredObject::RELEASE_BUILD;
+#endif
+}
