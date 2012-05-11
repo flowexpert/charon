ce7ed4371dbf2ab9b47f84d634f8a1902dc058c1
Jens-Malte Gottfried <jmgottfried@web.de>
2012-05-11 16:34:26 +0200

    reworked installing translators

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <QLibraryInfo>
 #include <QLocale>
 #include <QTranslator>
+#include <QDir>
 #include "Wizard.h"
 
 int main(int argc, char *argv[])
@@ -10,22 +11,33 @@ int main(int argc, char *argv[])
 	app.setOrganizationName("Heidelberg Collaboratory for Image Processing");
 	app.setApplicationName("TemplateGenerator");
 
+	// translator search paths
+	QStringList tPaths;
+	tPaths << QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+#ifdef TEMPLATE_GENERATOR_QM_DIR
+	tPaths << TEMPLATE_GENERATOR_QM_DIR;
+#endif
+	tPaths << QCoreApplication::applicationDirPath();
+
+	// qt system translator
 	// translation of qt dialog buttons (apply, close etc.)
 	QTranslator qtTranslator;
-	qtTranslator.load(
-		"qt_" + QLocale::system().name(),
-		QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	QString tName = "qt_" + QLocale::system().name();
+	foreach (QString tPath, tPaths) {
+		if (qtTranslator.load(tName, tPath)) {
+			break;
+		}
+	}
 	app.installTranslator(&qtTranslator);
 
 	// translation of tr(...) commands
 	QTranslator translator;
-	translator.load(
-		"charon-templates_" + QLocale::system().name(),
-#ifdef TEMPLATE_GENERATOR_QM_DIR
-		TEMPLATE_GENERATOR_QM_DIR);
-#else
-		QCoreApplication::applicationDirPath());
-#endif
+	tName = "charon-templates_" + QLocale::system().name();
+	foreach (QString tPath, tPaths) {
+		if (translator.load(tName, tPath)) {
+			break;
+		}
+	}
 	app.installTranslator(&translator);
 
 	Q_INIT_RESOURCE(resource);
