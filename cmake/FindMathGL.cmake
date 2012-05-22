06abf59dcaff8ef3fea2dbffbe1621dee5476c05
Stephan Meister <stephan.meister@iwr.uni-heidelberg.de>
2012-05-22 11:51:54 +0200

    changed FindMathGL to use consistent capitalization and FindPackageHandleStandardArgs, added MSVC specific compiler flag for MathGL

--- cmake/FindMathGL.cmake
+++ cmake/FindMathGL.cmake
@@ -8,30 +8,31 @@
 # This file is part of charon-free.
 #
 
-FIND_PATH(MATHGL_INCLUDE_DIR NAMES mgl2/mgl.h
+FIND_PATH(MathGL_INCLUDE_DIR NAMES mgl2/mgl.h
 	PATHS
 	/usr/local/include
 	/usr/include
 )
 
-FIND_LIBRARY(MATHGL_LIB NAMES mgl
+FIND_LIBRARY(MathGL_MGL_LIB NAMES mgl
 	PATHS
 	/usr/local/lib
 	/usr/lib
 )
 
-FIND_LIBRARY(MATHGLWND_LIB NAMES mgl-wnd
+FIND_LIBRARY(MathGL_MGL-WND_LIB NAMES mgl-wnd
 	PATHS
 	/usr/local/lib
 	/usr/lib
 )
 
-IF(MATHGL_INCLUDE_DIR AND MATHGL_LIB AND MATHGLWND_LIB)
-	SET(MATHGL_FOUND TRUE)
-	SET(MATHGL_LIBS
-		${MATHGL_LIB}
-		${MATHGLWND_LIB}
-	)
-	MESSAGE( STATUS "Found MathGL library, version 2.0" )
-ENDIF(MATHGL_INCLUDE_DIR AND MATHGL_LIB AND MATHGLWND_LIB)
+IF(MathGL_MGL_LIB AND MathGL_MGL-WND_LIB)
+	SET(MathGL_LIBS ${MathGL_MGL-WND_LIB} ${MathGL_MGL_LIB})
+ENDIF(MathGL_MGL_LIB AND MathGL_MGL-WND_LIB)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MathGL DEFAULT_MSG MathGL_MGL_LIB MathGL_MGL-WND_LIB MathGL_INCLUDE_DIR)
+
+mark_as_advanced(MathGL_MGL_LIB MathGL_MGL-WND_LIB MathGL_INCLUDE_DIR )
+
 
