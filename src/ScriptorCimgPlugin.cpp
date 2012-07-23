c6e1ef679cabb10e55effb39e8fc45d926f427f1
Jens-Malte Gottfried <jmgottfried@web.de>
2012-07-23 16:43:23 +0200

    fixed deprecated warning

--- src/ScriptorCimgPlugin.cpp
+++ src/ScriptorCimgPlugin.cpp
@@ -31,8 +31,8 @@
 #include <charon-utils/Scriptor/ScriptorCimgPlugin.hxx>
 
 /// Creates an instance of the plugin
-extern "C" ScriptorCimgPlugin_DECLDIR ParameteredObject*
-		create(const std::string & name, template_type t) {
+extern "C" ScriptorCimgPlugin_DECLDIR ParameteredObject* create(
+		const std::string & name, ParameteredObject::template_type t) {
 	switch(t) {
 	case ParameteredObject::TYPE_DOUBLE:
 		return new TYPE<double>(name);
