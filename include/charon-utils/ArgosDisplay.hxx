997be68a0d68696a6da22d5a1e30b825f2cac7cf
gmwangi <gerald.mwangi@gmx.de>
2012-11-02 10:40:03 +0100

    Fixed dynamic cast issue of ArgosDisplay related to VirtualSlots

--- include/charon-utils/ArgosDisplay.hxx
+++ include/charon-utils/ArgosDisplay.hxx
@@ -143,8 +143,21 @@ void ArgosDisplayPlugin<T>::execute() {
 		std::string name = (*it)->getParent().getName() + "." + (*it)->getName() ;
 		// dynamic_cast fails for unknown reasons, therefore this
 		// horrible piece of code
+#ifdef SUPERNODES_BRANCH
+		Slot* sl=(Slot*)(*it);
+		OutputSlotIntf* out=dynamic_cast<OutputSlotIntf*>(sl);
+
+		if(!out)
+			vigra_fail(
+					"cast of vigra::MultiArrayView failed! "
+					"In/Output slot may be invalid!");
+		const OutputSlot<vigra::MultiArray<5,T> >* temp =
+			dynamic_cast<const OutputSlot<vigra::MultiArray<5, T> >*>(out->getThisPointer());
+#else
 		const OutputSlot<vigra::MultiArray<5,T> >* temp =
 			dynamic_cast<const OutputSlot<vigra::MultiArray<5, T> >*>(*it);
+#endif
+
 		if(!temp)
 			vigra_fail(
 					"cast of vigra::MultiArrayView failed! "
@@ -167,8 +180,23 @@ void ArgosDisplayPlugin<T>::execute() {
 	for(cit = _cimgIn.begin(); cit != cend ; cit++) {
 		std::string name = (*cit)->getParent().getName() + "."
 				+ (*cit)->getName() ;
+
+#ifdef SUPERNODES_BRANCH
+		Slot* sl=(Slot*)(*cit);
+		OutputSlotIntf* out=dynamic_cast<OutputSlotIntf*>(sl);
+		if(!out) {
+			throw std::runtime_error(
+					this->getClassName() + " : " + this->getName()
+						+ " : cast of cimg_library::CImgList failed! "
+					"In/Output slot \"" + name + "\" may be invalid!");
+		}
+		const OutputSlot<cimg_library::CImgList<T> >* temp =
+			dynamic_cast<const OutputSlot<cimg_library::CImgList<T> >*>(out->getThisPointer());
+#else
 		const OutputSlot<cimg_library::CImgList<T> >* temp =
 			dynamic_cast<const OutputSlot<cimg_library::CImgList<T> >*>(*cit);
+#endif
+
 		if(!temp) {
 			throw std::runtime_error(
 					this->getClassName() + " : " + this->getName()
