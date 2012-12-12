7370ef96d9b6aa0720408b6cc116dc5c303736ed
Jens-Malte Gottfried <jmgottfried@web.de>
2012-12-12 20:31:49 +0100

    renamed CDash project to charon-flow

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,10 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "charon")
+set(CTEST_PROJECT_NAME "charon-flow")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "charon-suite.sourceforge.net")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=charon")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=charon-flow")
 set(CTEST_DROP_SITE_CDASH TRUE)
