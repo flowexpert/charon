ee258315a92362f7ce9096a8f221e7e743dc0a7c
Michael Baron <michael.baron@iwr.uni-heidelberg.de>
2013-01-14 15:22:21 +0100

    simplified Warper

--- include/charon/Warper.hxx
+++ include/charon/Warper.hxx
@@ -73,35 +73,32 @@ T Warper<T>::getX( T c, T x, T, T xMotion, T yMotion )
 		x += _motion[0].atXYZC(xMotion,yMotion,0,0);
 	}
 
-	// asymmetric behavior needed to emulate DS_s implementation  // (!!)
-	if (c == ONLY_CHECK_BOUNDS) {                                 // (!!)
-		if (x >= _dimX-1) return 0;                           // (!!)
-	}                                                             // (!!)
+	// check, if x is within image bounds
+	if (c == ONLY_CHECK_BOUNDS) {
+		if ((x >= 0) && (x <= _dimX-1)) return 1;
+		return 0;
+	}
 
-	// mirroring at image boundaries, if new x is out of bounds
 	if (mirror())
 	{
+		// mirroring at image boundaries, if new x is out of bounds
 		if (x < 0) {
-			if (c == ONLY_CHECK_BOUNDS) return 0;
 			return -x -1 ;
-		} else if (x >= _dimX) {
-			if (c == ONLY_CHECK_BOUNDS) return 0;
+		} else if (x > _dimX-1) {
 			return 2*_dimX -x -1 ;
+		} else {
+			return x;
 		}
-		if (c == ONLY_CHECK_BOUNDS) return 1;
-		return x;
+	} else {
+		// radial extrapolation, if new x is out of bounds
+		if (x < 0) {
+			return 0;
+		} else if (x > _dimX-1) {
+			return _dimX-1;
+		} else {
+			return x;
+		}
 	}
-
-	// radial extrapolation, if new x is out of bounds
-	if (x < 0) {
-		if (c == ONLY_CHECK_BOUNDS) return 0;
-		return 0;
-	} else if (x >= _dimX) {
-		if (c == ONLY_CHECK_BOUNDS) return 0;
-		return _dimX-1;
-	}
-	if (c == ONLY_CHECK_BOUNDS) return 1;
-	return x;
 }
 
 template <typename T>
@@ -112,35 +109,32 @@ T Warper<T>::getY( T c, T, T y, T xMotion, T yMotion )
 		y += _motion[1].atXYZC(xMotion,yMotion,0,0);
 	}
 
-	// asymmetric behavior needed to emulate DS_s implementation  // (!!)
-	if (c == ONLY_CHECK_BOUNDS) {                                 // (!!)
-		if (y >= _dimY-1) return 0;                           // (!!)
-	}                                                             // (!!)
+	// check, if x is within image bounds
+	if (c == ONLY_CHECK_BOUNDS) {
+		if ((y >= 0) && (y <= _dimX-1)) return 1;
+		return 0;
+	}
 
-	// boundary handling (mirror)
 	if (mirror())
 	{
+		// boundary handling (mirror)
 		if (y < 0) {
-			if (c == ONLY_CHECK_BOUNDS) return 0;
 			return -y -1 ;
-		} else if (y >= _dimY) {
-			if (c == ONLY_CHECK_BOUNDS) return 0;
+		} else if (y >= _dimY-1) {
 			return 2*_dimY -y -1 ;
+		} else {
+			return y;
 		}
-		if (c == ONLY_CHECK_BOUNDS) return 1;
-		return y;
+	} else {
+		// boundary handling (radial)
+		if (y < 0) {
+			return 0;
+		} else if (y > _dimY-1) {
+			return _dimY-1;
+		} else {
+			return y;
+		}
 	}
-
-	// boundary handling (radial)
-	if (y < 0) {
-		if (c == ONLY_CHECK_BOUNDS) return 0;
-		return 0;
-	} else if (y >= _dimY) {
-		if (c == ONLY_CHECK_BOUNDS) return 0;
-		return _dimY-1;
-	}
-	if (c == ONLY_CHECK_BOUNDS) return 1;
-	return y;
 }
 
 #endif
