61fb6ce9a12d7ed367963e2fa1e7d86b45707bfd
Stephan Meister <stephan.meister@iwr.uni-heidelberg.de>
2013-04-30 14:14:04 +0200

    removed residualplotwidget (was unused and incompatible with newer qwt6) made QuiverPlot compatible with MathGL 2.1+ and removed support for MathGL 2.0

--- cmake/FindMathGL.cmake
+++ cmake/FindMathGL.cmake
@@ -13,6 +13,15 @@ FIND_PATH(MathGL_INCLUDE_DIR NAMES mgl2/mgl.h
 	/usr/local/include
 	/usr/include
 )
+SET(MathGL_VERSION)
+IF(MathGL_INCLUDE_DIR)
+	FILE(READ ${MathGL_INCLUDE_DIR}/mgl2/define.h VERSION_FILE)
+	STRING(REGEX MATCH "MGL_VER2[ \t\n]+([0-9\\.]+)" _TMP_VAR ${VERSION_FILE})
+	SET(MathGL_VERSION ${CMAKE_MATCH_1})
+	UNSET(_TMP_VAR)
+ENDIF(MathGL_INCLUDE_DIR)
+	MESSAGE(STATUS "Found MathGL Version: 2.${MathGL_VERSION}")
+
 
 FIND_LIBRARY(MathGL_MGL_LIB NAMES mgl
 	PATHS
@@ -26,13 +35,19 @@ FIND_LIBRARY(MathGL_MGL-WND_LIB NAMES mgl-wnd
 	/usr/lib
 )
 
-IF(MathGL_MGL_LIB AND MathGL_MGL-WND_LIB)
-	SET(MathGL_LIBS ${MathGL_MGL-WND_LIB} ${MathGL_MGL_LIB})
-ENDIF(MathGL_MGL_LIB AND MathGL_MGL-WND_LIB)
+IF(MathGL_MGL_LIB)
+	SET(MathGL_LIBS ${MathGL_MGL_LIB})
+ENDIF(MathGL_MGL_LIB)
+
+IF(MathGL_MGL_WND_LIB)
+	LIST(APPEND MathGL_LIBS ${MathGL_MGL_WND_LIB})
+ENDIF(MathGL_MGL_WND_LIB)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MathGL DEFAULT_MSG MathGL_MGL_LIB MathGL_MGL-WND_LIB MathGL_INCLUDE_DIR)
+find_package_handle_standard_args(
+		MathGL 
+		REQUIRED_VARS MathGL_MGL_LIB MathGL_INCLUDE_DIR
+		VERSION_VAR MathGL_VERSION
+)
 
-mark_as_advanced(MathGL_MGL_LIB MathGL_MGL-WND_LIB MathGL_INCLUDE_DIR )
-
-
+mark_as_advanced(MathGL_MGL_LIB MathGL_MGL-WND_LIB MathGL_INCLUDE_DIR MathGL_VERSION)
