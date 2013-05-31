afec79f5a67415a85fa546998b55c69f06a6e481
Jens-Malte Gottfried <jmgottfried@web.de>
2013-05-31 11:52:10 +0200

    added html copy

--- src/QCopyListView.cpp
+++ src/QCopyListView.cpp
@@ -39,17 +39,56 @@ void QCopyListView::keyPressEvent(QKeyEvent* ev) {
 	// based on the QT code from AbstractItemView::keyPressEvent
 #if !defined(QT_NO_CLIPBOARD) && !defined(QT_NO_SHORTCUT)
 	if (ev == QKeySequence::Copy) {
-		QStringList lines;
+		QStringList linesT,linesH;
 		QModelIndexList selection = selectedIndexes();
 		QListIterator<QModelIndex> selIter(selection);
 		while (selIter.hasNext()) {
 			QModelIndex curIdx = selIter.next();
-			QVariant curData = model()->data(curIdx,Qt::DisplayRole);
+			QVariant curData; QString colStyle;
+			curData = model()->data(curIdx,Qt::ForegroundRole);
+			if (curData.type() == QVariant::Brush) {
+				QBrush brush = qvariant_cast<QBrush>(curData);
+				QString color = brush.color().name();
+				if (!color.isEmpty()) {
+					colStyle += QString("color:%1;").arg(color);
+				}
+			}
+			curData = model()->data(curIdx,Qt::BackgroundRole);
+			if (curData.type() == QVariant::Brush) {
+				QBrush brush = qvariant_cast<QBrush>(curData);
+				QString color = brush.color().name();
+				if (!color.isEmpty()) {
+					colStyle += QString("background-color:%1;").arg(color);
+				}
+			}
+			curData = model()->data(curIdx,Qt::DisplayRole);
 			if (curData.type() == QVariant::String) {
-				lines << curData.toString();
+				QString curLine = curData.toString();
+				linesT << curLine;
+				if (colStyle.isEmpty()) {
+					linesH << curLine;
+				}
+				else {
+					linesH << QString("<span style=\"%1\">%2</span>")
+						.arg(colStyle).arg(curLine);
+				}
 			}
 		}
-		QApplication::clipboard()->setText(lines.join("\n"));
+		/*
+		// analyze old conent
+		const QMimeData* oldContent = QApplication::clipboard()->mimeData();
+		qDebug("Formats: %s\n",
+			oldContent->formats().join(",").toLocal8Bit().constData());
+		qDebug("Html content: %s\n",
+			oldContent->html().toLocal8Bit().constData());
+		QVariant tmp = oldContent->data("text/html");
+		qDebug("moz_html: %s\n", tmp.toString().toLocal8Bit().constData());
+		*/
+
+		QMimeData* clipData = new QMimeData;
+		clipData->setText(linesT.join("\n"));
+		clipData->setHtml(linesH.join("<br>"));
+		QApplication::clipboard()->setMimeData(clipData);
 		ev->accept();
 	}
 	else {
